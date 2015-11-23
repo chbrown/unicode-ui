5ed2c2cf8a0a59712cb941c64d2f5f6be9a52de4
Christopher Brown <io@henrian.com>
2015-11-22 22:13:45 -0600

    Remove type_declarations/

--- type_declarations/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="DefinitelyTyped/angularjs/angular.d.ts" />
-/// <reference path="DefinitelyTyped/unorm/unorm.d.ts" />
-/// <reference path="DefinitelyTyped/virtual-dom/virtual-dom.d.ts" />
-
-/// <reference path="../node_modules/unidata/unidata.d.ts" />
